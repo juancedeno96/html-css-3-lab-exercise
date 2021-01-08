a1938884be80a7ab77c79e465a4b638ebf4a0e0f
juancedeno96 <cedenojj7@gmail.com>
2021-01-07 23:12:48 -0700

    in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Component } from 'react';
+import './App.css';
+import logo from './iconfinder-icon.svg'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      dropdownView: false
+    }
+  }
+  toggleDropdown = () => {
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+render(){
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+     <section className='main'>
+       
+     <header className='header'>
+        <h2 className = 'start'>Start Bootstrap</h2>
+        <nav className='navbar'>
+        <span>Services</span>
+        <span> Portfolio</span>
+        <span>About</span>
+        <span>Team</span>
+        <span>Contact</span>
+        </nav>
+        <div className="dropdown" onClick={this.toggleDropdown}>
+        <img src = {logo}/>
+</div>
+{this.state.dropdownView
+?(
+  <nav className = 'mobile-menu'>
+    <span>Services</span>
+        <span> Portfolio</span>
+        <span>About</span>
+        <span>Team</span>
+        <span>Contact</span>
+  </nav>
+): null}
       </header>
+
+      <section className='middle'>
+          <h2 id = 'Welcome'>Welcome to our Studio!</h2>
+          <h1 id = "Meet">It's Nice To Meet You</h1>
+          <h2 id = 'Tell'><p>Tell Me More</p></h2>
+      </section>
+     </section>
     </div>
   );
+  }
 }
 
 export default App;
